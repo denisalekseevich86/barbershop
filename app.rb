bcfd3cd37d0f2f5c7fbbd3e4e861b4f7bdb22578
denisalekseevich86 <denisalekseevich86@gmail.com>
2020-11-24 15:24:37 +0300

    barbershop

---
+++ app.rb
@@ -0,0 +1,57 @@
+
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+configure do 
+	db = get_db
+	db.execute 'CREATE TABLE "Users"( "id" INTEGER PRIMARY KEY AUTOINCREMENT, "name" TEXT , "phone" TEXT, "note" TEXT, "barber" TEXT)'
+	
+end
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/About' do 
+	erb :About 
+end 
+
+get '/visit' do
+
+	erb :visit 	
+end 
+
+post '/visit' do 
+	@name   =  params[:name]
+	@phone  =  params[:phone]
+	@note   =  params[:note]
+	@barber =  params[:barber]
+
+	hh = {:name => 'Ведите имя', 
+		:phone => 'Телефон', 
+		:note => 'время' }
+
+	hh.each do |key , value|
+		if params[key] == ''
+			@error = hh[key]
+			return erb :visit 
+		end
+	end
+
+	db = get_db
+	db.execute 'insert into Users (name, phone, note, barber) 
+	values (?, ?, ?, ?)',[@name, @phone, @note, @barber]
+	erb "Ok! Name: #{@name}, phone: #{@phone}, время запси #{@note}, barber #{@barber}"		
+end 	
+
+get '/contact' do 
+
+	erb :contact
+ 
+end 
+
+def get_db
+	return SQLite3::Database.new 'barbershop.sqlite'
+end 
